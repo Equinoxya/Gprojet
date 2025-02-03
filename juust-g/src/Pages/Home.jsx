350ca5d78cfe5d5d6c4affa745eaa3c932fcf654
Ophélie BELLISSENS <obellissens1@gmail.com>
2025-02-03 17:17:26 +0100

    dsq

--- juust-g/src/Pages/Home.jsx
+++ juust-g/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div>
             <Charly />
             <Navbar />
-            <Background />
+            <Background/>
             <Footer />
             
         </div> 
